e2f7d899114ef22ae45c2c4a2b75b979bf55b76a
Zelalem1222 <zelalemfissha394@gmail.com>
2022-12-09 15:24:05 +0300

    Modify visiblity toggle

--- src/playground/visiblityToggle.js
+++ src/playground/visiblityToggle.js
@@ -1,24 +1,54 @@
 const root = document.getElementById('root') 
-let visibility = false
+// let visibility = false
 
-const handleClick = () => {
-    visibility = !visibility
-    renderer()
-}
+// const handleClick = () => {
+//     visibility = !visibility
+//     renderer()
+// }
 
-const renderer = () => {
-    const template = (
-        <div>
-          <h1>Hello</h1>
-          <button onClick={handleClick}>
-           { visibility ? 'Hide Details':  'Show Details'  }
-          </button>
-          {visibility && (
-            <p>This is some details you must see.</p>
-          )}
-        </div>
-    )
+// const renderer = () => {
+//     const template = (
+//         <div>
+//           <h1>Hello</h1>
+//           <button onClick={handleClick}>
+//            { visibility ? 'Hide Details':  'Show Details'  }
+//           </button>
+//           {visibility && (
+//             <p>This is some details you must see.</p>
+//           )}
+//         </div>
+//     )
     
-ReactDOM.render(template , root)
+// ReactDOM.render(template , root)
+// }
+// renderer()
+
+class Visiblity extends React.Component {
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+    this.state = {
+      visiblity: false
+    }
+  }
+
+  handleClick(){
+    this.setState((prevState) => {
+      return {
+        visiblity: !prevState.visiblity
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Hello</h1>
+        <button onClick={this.handleClick}>{this.state.visiblity ? 'Hide Details' : 'Show Details'}</button>
+        {this.state.visiblity && <p>This is some details you must see.</p>}
+      </div>
+    )
+  }
 }
-renderer()
+
+ReactDOM.render(<Visiblity /> , root)
